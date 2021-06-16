901e0bc3f68702980844f090f5715513ecf8476f
oi628 <85979003+oi628@users.noreply.github.com>
2021-06-16 09:34:47 +0530

    Add files via upload

---
+++ project/js/script.js
@@ -0,0 +1,13 @@
+$('#giftButton').click(function(){
+	$("#giftPage").fadeOut(1500);
+	$('#wishH1').addClass('animation-start');
+	var song = document.getElementById('audio');
+	song.currentTime = 0;
+	song.play();
+	setTimeout(function() {
+      $("#giftPage").fadeIn(1500);
+	  setTimeout(function() {
+		$('#wishH1').removeClass('animation-start');
+	  },1500);
+    }, 30000);
+});
